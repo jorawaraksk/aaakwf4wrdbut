b18b4a75e70357df3672fff86378dc00f00e4119
jorawaraksk <jorawarzu@proton.me>
2025-07-29 02:57:45 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,9 +7,9 @@ from os import environ
 class Config:
     API_ID = int(environ.get("API_ID", "28983177"))
     API_HASH = environ.get("API_HASH", "a0da2f0c10923e1c080fd86ab4c04051")
-    BOT_TOKEN = environ.get("BOT_TOKEN", "6687586800:AAGYLoUYayq_5keqNhIaXtiW1Tg2BLxhaSI") 
+    BOT_TOKEN = environ.get("BOT_TOKEN", "6524339957:AAHDCtzmBzGbg8tbpz5xW2s-jFuY77n_cHw") 
     BOT_SESSION = environ.get("BOT_SESSION", "vjbot") 
-    DATABASE_URI = environ.get("DATABASE_URI", "mongodb+srv://siksha:Movie12345@cluster0.f8sws18.mongodb.net")
+    DATABASE_URI = environ.get("DATABASE_URI", "mongodb+srv://zuzoo:Movie12345@cluster0.y7xfsuh.mongodb.net")
     DATABASE_NAME = environ.get("DATABASE_NAME", "vj-forward-bot")
     BOT_OWNER = int(environ.get("BOT_OWNER", "5868426717"))
 
